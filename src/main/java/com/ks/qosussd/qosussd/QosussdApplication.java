69f06118fa3d1dba44b86c8ce78a773d3f2f4ef5
koyaja <koyajawil@gmail.com>
2019-04-24 23:50:38 +0100

    initial commit

---
+++ src/main/java/com/ks/qosussd/qosussd/QosussdApplication.java
@@ -0,0 +1,13 @@
+package com.ks.qosussd.qosussd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QosussdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QosussdApplication.class, args);
+	}
+
+}
